eb56d2ccec64784d6c57c48cd16304b601e5beae
Md.Asib khan <108424194+coderasib@users.noreply.github.com>
2022-07-07 00:09:00 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,90 @@
+(function ($) {
+    "use strict";
+  
+    $('.brand-active').owlCarousel({
+        loop:true,
+        margin:30,
+        smartSpeed: 9000,
+        autoplay: 1000,
+        nav:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+        responsive:{
+            0:{
+                items:5
+            },
+            768:{
+                items:5
+            },
+            992:{
+                items:5
+            },
+            1200:{
+                items:5
+            }
+        }
+    })  
+    $('.slider-active').owlCarousel({
+        loop:true,
+        margin:0,
+
+        nav:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:3
+            }
+        }
+    })
+
+
+
+    $(document).ready(function() {
+  
+        /* Small card slider */ 
+        $('.slider').slick({
+          arrows: false,
+          dots: false,
+          slidesToShow: 2
+        });
+        
+        const slider = $(".slider");
+        var scrollCount = null;
+      var scroll= null;
+      
+        slider.on('wheel', (function(e) {
+          e.preventDefault();
+          
+          clearTimeout(scroll);
+          scroll = setTimeout(function(){scrollCount=0;}, 200);
+          if(scrollCount) return 0;
+          scrollCount=1;
+      
+          if (e.originalEvent.deltaY < 0) {
+            $(this).slick('slickNext');
+          } else {
+            $(this).slick('slickPrev');
+          }
+        }));
+       
+        /* Big cards slider */ 
+        $('.slider-big-cards').slick({
+          arrows: false,
+          dots: false,
+          slidesToShow: 2
+        });
+      });
+        
+        
+    
+		
+		
+}(jQuery));	
